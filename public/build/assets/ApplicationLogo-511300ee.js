90b4993f5d730c5fe9691ca3a58b7e85cae8097f
nagaraj161400 <nagaraj161400@gmail.com>
2024-01-22 22:44:41 +0530

    last build

--- public/build/assets/ApplicationLogo-511300ee.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-15a71199.js";function p(r){return o.jsx(o.Fragment,{})}export{p as A};
---
+++ public/build/assets/ApplicationLogo-ba8eb19d.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-1c6321e0.js";function p(r){return o.jsx(o.Fragment,{})}export{p as A};
